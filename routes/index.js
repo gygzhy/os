6ef193d60d7f959012a3030cf6745001b0278737
Bobby.c <huanbin.c@gmail.com>
2015-06-28 17:07:26 +0800

    Adding bower, polymer for front-end

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var java = require('java');
+
+java.classpath.push("../node_modules/java/test/commons-lang3-3.1.jar");
+
+var array = java.newArray('java.lang.String', ["fuck", "this", "shit"]);
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: array[0] });
 });
 
 module.exports = router;
